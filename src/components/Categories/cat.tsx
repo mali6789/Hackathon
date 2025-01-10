c232909a051bfa38b865ad21e8a6028129113f0c
unknown <ia761288@gmail.com>
2025-01-10 20:26:18 +0500

    Update: project changes

--- src/components/Categories/cat.tsx
+++ src/components/Categories/cat.tsx
@@ -9,7 +9,7 @@ export default function TopCategories() {
             <div className="w-[424px] h-[424px] absolute top-[84px] opacity-[20%]"></div>
             <div className="w-[424px] h-[424px] absolute top-[84px] left-[448px]">
                 <div>
-                    <Image src="/image (4).png" alt="Wing Chair" width={424} height={424} />
+                    <Image src={"/image-4.png"} alt="Wing Chair" width={424} height={424} />
                     <div className="w-[424px] h-[85px] absolute top-[339px] rounded-br-[10px] rounded-bl-[10px] p-[20px] flex flex-col gap-[8px] bg-[#000000B2]">
                         <div className="f-['inter'] text-[20px] leading-[22px] text-white">Wing Chair</div>
                         <div className="f-['inter'] text-[14px] font-[400] leading-[15.4px] opacity-[70%] text-white">3,584 Products</div>
@@ -19,7 +19,7 @@ export default function TopCategories() {
 
             <div className="w-[424px] h-[424px] absolute top-[84px] left-[896px]">
                 <div>
-                    <Image src="/image (5).png" alt="Wooden Chair" width={424} height={424} />
+                    <Image src="/image-5.png" alt="Wooden Chair" width={424} height={424} />
                     <div className="w-[424px] h-[85px] absolute top-[339px] rounded-br-[10px] rounded-bl-[10px] p-[20px] flex flex-col gap-[8px] bg-[#000000B2]">
                         <div className="f-['inter'] text-[20px] leading-[22px] text-white">Wooden Chair</div>
                         <div className="f-['inter'] text-[14px] font-[400] leading-[15.4px] opacity-[70%] text-white">157 Products</div>
@@ -29,7 +29,7 @@ export default function TopCategories() {
 
             <div className="w-[424px] h-[424px] absolute top-[84px] left-[1344px]">
                 <div>
-                    <Image src="/image (7).png" alt="Desk Chair" width={424} height={424} />
+                    <Image src="/image-7.png" alt="Desk Chair" width={424} height={424} />
                     <div className="w-[424px] h-[85px] absolute top-[339px] rounded-br-[10px] rounded-bl-[10px] p-[20px] flex flex-col gap-[8px] bg-[#000000B2]">
                         <div className="f-['inter'] text-[20px] leading-[22px] text-white">Desk Chair</div>
                         <div className="f-['inter'] text-[14px] font-[400] leading-[15.4px] opacity-[70%] text-white">154 Products</div>
