08031ca92c717a98664eb4a4c6dea8cf1d073cc6
unknown <ia761288@gmail.com>
2025-02-02 15:32:16 +0500

    UPDATED

--- src/components/Navigation/Links/links.tsx
+++ src/components/Navigation/Links/links.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function NavbarLinks(){
     return(
-        <div className="bg-white lg:h-[74px] lg:w-full flex justify-between items-center py-[14px] px-[30px] md:px-[50px] lg:px-[300px]" style={{ boxShadow: '1px 1px 0px 0px #E1E3E5' }}>
+        <div className="bg-white lg:h-[74px] w-[440px] lg:w-full flex justify-between lg:items-center py-[14px] px-[20px] md:px-[50px] lg:px-[300px]" style={{ boxShadow: '1px 1px 0px 0px #E1E3E5' }}>
              <div className="h-[15px] flex gap-[20px] lg:gap-[32px] font-inter text-[14px] font-medium leading-[15.4px] text-left">
                  <Link className="text-[#636270] hover:text-[#007580]" href={"/"}>Home</Link>
                  <Link className="text-[#636270]" href={"#"}>Shop</Link>
--- src/components/Navigation/Middle/page.tsx
+++ src/components/Navigation/Middle/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function NavbarMiddle() {
     return (
-      <div className="bg-white lg:h-[84px] w-[440px] lg:w-full py-[20px] px-[50px] lg:px-[300px] flex justify-between">
+      <div className="bg-white lg:h-[84px] w-[440px] lg:w-full py-[20px] px-[40px] lg:px-[300px] flex justify-between">
         <div><Image src="/Logo.png" alt="Product"  width={166} height={40}/></div>
         <Link href={"/chart"}> <Image src="/User Links.png" alt="Product" width={120} height={44} /></Link>
       </div>
